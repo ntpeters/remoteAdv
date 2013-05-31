4d5c39247e24b7a118fab09a9b2c2160c2b63e5a
ntpeters <ntpeters@mtu.edu>
2013-05-30 23:52:02 -0500

    Completed logging function

--- src/server.c
+++ src/server.c
@@ -11,16 +11,20 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <time.h>
+
 
 /* 141.219.153.205 for wopr     */
 /* 141.219.153.206 for guardian */
 
 // Global Variables
-int dbgLevel = 0;                  // 0 = none, 1 = basic , 2 = max TODO: Will have to change by signal. 
+int dbgLevel = 2;                  // Defined by user: 0 = none, 1 = basic , 2 = max TODO: Will have to change by signal. 
 int server_portnumber = 51739;     // Port must be constant due to the nature of this project
+char* logFile = "log.txt";
 
 // Function Prototypes
-void writeLog( int dbglvl , char* str );
+void writeLog( int loglvl , char* str );
+char* getDateString();
 
 // Output is standard out
 int main( int argc , char* argv[] ){
@@ -46,7 +50,7 @@ int main( int argc , char* argv[] ){
      printf( "%d\n", s1.sin_port );                                 /* Print the port number */
      signal( SIGCHLD , SIG_IGN );                                   /* So we don't wait on zombies */
      listen( listenFD , 512 );                                      /* I don't think we'll hit 512 clients.. */
-     
+
      /* ---- Keep listening for clients ---- */
      int isDone = 1;
      while(1){
@@ -83,8 +87,64 @@ int main( int argc , char* argv[] ){
 }
 
 
-// Writes to standard out
-void writeLog( int dbglvl , char* str ){
-     if(dbglvl <= dbgLevel )
-          write( 0 , str , strlen( str ) );
+/*
+     Writes output to defined logfile with date/time stamp and
+     associated log level.
+
+     Logging Levels:
+     -1 : Error
+     0  : Info
+     1  : Debug-Warnings
+     2  : Debug-All
+*/
+void writeLog( int loglvl , char* str ){
+     // Open the log file
+     int log = open( logFile, O_CREAT | O_APPEND | O_RDWR, 0664);
+
+     // Get current date/time
+     char* date = getDateString();
+
+     // Allocate message variable
+     int msgSize = strlen( str ) + strlen ( date ) + sizeof( strerror( errno ) )+ 25;
+     char* msg = malloc( msgSize );
+
+     if( loglvl == -1 ) {
+          // Compose log message
+          sprintf( msg, "%s\tERROR : %s\n", date, str);
+          // If errno is anything other than "Success", write it to the log.
+          if( errno != 0 ) {
+               sprintf( msg, "%s\n", strerror( errno ) );
+          }
+          // Write message to log
+          write( log, msg, strlen( msg ) );
+     } else if(loglvl == 0 ) {
+          sprintf( msg, "%s\tINFO  : %s\n", date, str);
+          write( log, msg, strlen( msg ) );
+     } else if( loglvl == 1 && dbgLevel >= 1 ) {
+          sprintf( msg, "%s\tWARN  : %s\n", date, str);
+          write( log, msg, strlen( msg ) );
+     } else if( loglvl == 2 && dbgLevel >= 2 ) {
+          sprintf( msg, "%s\tDEBUG : %s\n", date, str);
+          write( log, msg, strlen( msg ) );
+     }
+
+     close(log);
+     free(msg);
+}
+
+char* getDateString() {
+     time_t t = time ( NULL );
+     struct tm *timeinfo = localtime( &t );
+     char* date = malloc(100);
+
+     int year = timeinfo->tm_year + 1900;
+     int month = timeinfo->tm_mon + 1;
+     int day = timeinfo->tm_mday;
+     int hour = timeinfo->tm_hour;
+     int min = timeinfo->tm_min;
+     int sec = timeinfo->tm_sec;
+
+     sprintf( date, "[%d-%d-%d %d:%d:%d]", year, month, day, hour, min, sec);
+
+     return date;
 }
